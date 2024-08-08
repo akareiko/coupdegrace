75485a289292469df58dd3d682e325851f084a48
akareiko <140501296+akareiko@users.noreply.github.com>
2024-08-09 04:47:09 +0600

    added 3d card

--- coup/src/app/page.js
+++ coup/src/app/page.js
@@ -1,9 +1,10 @@
 import S3UploadForm from '@/app/components/S3UploadForm'
-
+import Card from '@/app/components/Card'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <S3UploadForm/>
+      <Card></Card>
     </main>
   );
 }
