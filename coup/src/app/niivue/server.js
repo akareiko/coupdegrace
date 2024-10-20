7bd09b1af8cd43e5feae60272fd05bb307919f54
akareiko <140501296+akareiko@users.noreply.github.com>
2024-10-20 14:59:34 +0900

    delete niivue native

--- coup/src/app/niivue/server.js
+++
@@ -1,9 +0,0 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import express from 'express'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
-const app = express()
-app.use(express.static(__dirname))
-app.listen(8888)
--- coup/src/app/visualization/page.js
+++ coup/src/app/visualization/page.js
@@ -1,11 +1,9 @@
 "use client";
 
-import NiivueViewer from "../niivue/src/niiviewer";
-
 export default function Home() {
     return (
       <main className="min-h-screen">
-        <NiivueViewer />
+        
       </main>
     );
   }
